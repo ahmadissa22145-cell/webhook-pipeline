bfc67852847649ed8c88afbfa78e24057b944386
ahmadissa22145-cell <ahmadissa22145@gmail.com>
2026-03-24 18:26:11 +0200

    refactor(pipeline-subscriber): remove checkSubscription controller and related logic

--- src/controllers/pipelineSubscriber.controller.ts
+++ src/controllers/pipelineSubscriber.controller.ts
@@ -86,37 +86,6 @@ export async function getSubscriptionByIdController(
   }
 }
 // ========================================
-export async function checkSubscriptionController(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
-  try {
-    const { pipelineId, subscriberId } = req.query as {
-      pipelineId: string;
-      subscriberId: string;
-    };
-
-    const trimmedPipelineId = pipelineId?.trim();
-    const trimmedSubscriberId = subscriberId?.trim();
-
-    if (!trimmedPipelineId || !trimmedSubscriberId) {
-      throw new BadRequestError("Pipeline ID and Subscriber ID is required");
-    }
-
-    const subscription = await checkSubscriptionService(
-      trimmedPipelineId,
-      trimmedSubscriberId,
-    );
-
-    res.status(200).json({
-      data: subscription,
-    });
-  } catch (error) {
-    next(error);
-  }
-}
-// ========================================
 export async function getSubscriptionByNameAndUrlController(
   req: Request,
   res: Response,
