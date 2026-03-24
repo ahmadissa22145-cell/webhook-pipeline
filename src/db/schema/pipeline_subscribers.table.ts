7dbd10188e79c34867c9c4df8d06671126edec10
ahmadissa22145-cell <ahmadissa22145@gmail.com>
2026-03-24 13:16:59 +0200

    fix: add missing file

--- src/db/schema/pipeline_subscribers.table.ts
+++ src/db/schema/pipeline_subscribers.table.ts
@@ -1,17 +1,19 @@
-import { pgTable, primaryKey, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, uniqueIndex } from "drizzle-orm/pg-core";
 import { pipelines } from "./pipelines.table.js";
 import { subscribers } from "./subscribers.table.js";
-import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { InferInsertModel, InferSelectModel, isNull } from "drizzle-orm";
 
 export const pipelineSubscribers = pgTable(
   "pipeline_subscribers",
   {
-    pipelineId: uuid("pipeline_id").references(() => pipelines.id, {
-      onDelete: "cascade",
-    }),
-    subscriberId: uuid("subscriber_id").references(() => subscribers.id, {
-      onDelete: "cascade",
-    }),
+    id: uuid("id").primaryKey().defaultRandom(),
+    pipelineId: uuid("pipeline_id")
+      .references(() => pipelines.id, { onDelete: "cascade" })
+      .notNull(),
+    subscriberId: uuid("subscriber_id")
+      .references(() => subscribers.id, { onDelete: "cascade" })
+      .notNull(),
+    unsubscribedAt: timestamp("unsubscribed_at"),
     createdAt: timestamp("created_at").notNull().defaultNow(),
     updatedAt: timestamp("updated_at")
       .notNull()
@@ -19,7 +21,9 @@ export const pipelineSubscribers = pgTable(
       .$onUpdate(() => new Date()),
   },
   (table) => ({
-    pk: primaryKey({ columns: [table.pipelineId, table.subscriberId] }),
+    uniqueActiveSubscription: uniqueIndex("unique_active_subscription")
+      .on(table.pipelineId, table.subscriberId)
+      .where(isNull(table.unsubscribedAt)),
   }),
 );
 
